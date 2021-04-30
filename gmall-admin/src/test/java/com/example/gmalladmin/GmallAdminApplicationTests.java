dae2d4737f70f872a5f0db331d6cc8c64453460c
DESKTOP-I9VQUA8\HP <huhaiyang7788@126.com>
2021-04-30 22:15:06 +0800

    提价

---
+++ gmall-admin/src/test/java/com/example/gmalladmin/GmallAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmalladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
